43559ef99804bda19a43edc851a48d03f11ec860
Alberto Benavente <albevi@gmail.com>
2015-02-19 09:12:49 +0100

    Update keyboard.js

--- www/keyboard.js
+++ www/keyboard.js
@@ -29,9 +29,20 @@ Keyboard.styleDark = function(dark) {
 };
 */
 
+// https://github.com/46cl/cordova-android-focus-plugin/blob/master/www/focus.js
+Keyboard.focus = function(element) {
+    element = element.length ? element[0] : element;
+    var elemRect = element.getBoundingClientRect(),
+        bodyRect = document.body.getBoundingClientRect(),
+        rect = {
+          top: elemRect.top - bodyRect.top,
+          left: elemRect.left - bodyRect.left,
+          right: elemRect.right - bodyRect.left,
+          bottom: elemRect.bottom - bodyRect.top
+        };
+    exec(null, null, "Keyboard", "focus", [rect]);
+};
+
 Keyboard.isVisible = false;
 
 module.exports = Keyboard;
-
-
-
